dff35bbdbf5a4252fbea63451e427375916eda95
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-09 15:27:44 +0100

    Footer component ready

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="border-t">
+            <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+                <Link href="/" className="w-52 flex items-center gap-1">
+                    <Image
+                        src="/assets/images/logo.svg"
+                        width={48}
+                        height={48}
+                        alt="Evento logo"
+                    />
+                    <p className="text-[24px] font-bold">Evento</p>
+                </Link>
+                <p>2024 Evento. all rights reserved</p>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
