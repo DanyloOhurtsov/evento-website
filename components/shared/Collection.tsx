24ec07a2c857157f503eb776ae1e65f000383a87
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-16 19:18:02 +0100

    Events Collection components
    Changes:
    - created types for prompt in Collection.tsx

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { IEvent } from "@/lib/database/models/event.model";
+import React from "react";
 
-const Collection = ({data,
-    emtyTitle,
-    emptyStateSubtext,
-    collectionType,
-    limit,
-    page,
-    totalPages}:) => {
-  return (
-    <div>Collection</div>
-  )
-}
+type CollectionProps = {
+  data: IEvent[];
 
-export default Collection
+  emtyTitle: string;
+  emptyStateSubtext: string;
+  limit: number;
+  page: number | string;
+  totalPages?: number;
+  urlParamName?: string;
+  collectionType: "Events_Organized" | "My_Tickets" | "All_Events";
+};
+
+const Collection = ({
+  data,
+
+  emtyTitle,
+  emptyStateSubtext,
+  collectionType,
+  limit,
+  page,
+  totalPages,
+}: CollectionProps) => {
+  return <div>Collection</div>;
+};
+
+export default Collection;
