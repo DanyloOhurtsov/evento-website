748ba3bde1875b1c10951fb711589b7a21353c96
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-16 19:14:49 +0100

    Event page ready | Begin of Collection comp

---
+++ components/shared/Collection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Collection = ({data,
+    emtyTitle,
+    emptyStateSubtext,
+    collectionType,
+    limit,
+    page,
+    totalPages}:) => {
+  return (
+    <div>Collection</div>
+  )
+}
+
+export default Collection
