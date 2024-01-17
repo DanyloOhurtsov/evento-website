7ad6bb088daef689c3bd8dfc914b6d1240d16ca0
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-17 13:21:47 +0100

    Card comp | Created Link for Editing

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import { formatDateTime } from "@/lib/utils";
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -11,8 +12,11 @@ type CardProps = {
 };
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-  
+  const isEventCreator = userId === event.organizer._id.toString();
+
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
@@ -23,6 +27,13 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
 
       {/* IS EVENT CREATOR */}
 
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+          <Link href={`/events/${event._id}/update`}>
+            <Image src={'/assets/icons/edit.svg'} alt="edit" width={20} height={20}/>
+          </Link>
+        </div>
+      )}
       <Link
         href={`/events/${event._id}`}
         className="flex  min-h-[230px] flex-col gap-3 p-5 md:gap-4"
@@ -45,13 +56,17 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </p>
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
-            {event.organizer.firstName}{' '}
-            {event.organizer.lastName}
+            {event.organizer.firstName} {event.organizer.lastName}
           </p>
           {hasOrderLink && (
             <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
               <p className="text-primary-500">Order details</p>
-              <Image src={'/assets/icons/arrow.svg'} alt="search" width={10} height={10} />
+              <Image
+                src={"/assets/icons/arrow.svg"}
+                alt="search"
+                width={10}
+                height={10}
+              />
             </Link>
           )}
         </div>
